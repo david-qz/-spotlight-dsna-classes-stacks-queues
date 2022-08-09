a373871ba1f231fa6bd2165bb6276ca2f0a529e9
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-08-09 11:49:24 -0700

    Test and implement Stack

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-class Stack {}
+class Stack {
+    #list;
 
-class Queue {}
+    constructor(initialList = []) {
+        this.#list = initialList;
+    }
+
+    get count() {
+        return this.#list.length;
+    }
+
+    push(item) {
+        this.#list.push(item);
+    }
+
+    pop() {
+        return this.#list.pop();
+    }
+
+    peek() {
+        return this.#list[this.#list.length - 1];
+    }
+}
+
+class Queue {
+
+}
 
 module.exports = { Stack, Queue };
--- index.test.js
+++ index.test.js
@@ -1,9 +1,26 @@
 const { Stack } = require("./index.js");
+
 describe("Stack Class", () => {
-  it("#push should add a new item to the stack ", () => {
+
+  it("#push should add a new item to the stack", () => {
+    const stack = new Stack();
+    stack.push('a');
+    stack.push('b');
+    expect(stack.count).toEqual(2);
+  });
+
+  it("#peek should return the top of the stack without modifying it", () => {
     const stack = new Stack();
     stack.push(1);
-    expect(stack.peek()).toEqual(1);
+    stack.push(2);
+    expect(stack.peek()).toEqual(2);
   });
-  // add more tests here...
+
+  it("#pop should remove the item at the top of the stack and remove it", () => {
+    const stack = new Stack();
+    stack.push(1);
+    stack.push(2);
+    expect(stack.count).toEqual(2);
+    expect(stack.peek()).toEqual(2);
+  });
 });
