0ae27fb4b537063668a479a252153df4ef619209
Aphenphos <MaxwillWinters@gmail.com>
2022-08-09 13:35:38 -0400

    Queue

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 class Stack {}
 
-class Queue {}
+class Queue {
+    #list;
+
+    constructor(initalList = []) {
+        this.#list = initalList
+    }
+
+    enqueue(item) {
+        this.#list.unshift(item)
+    }
+
+    deenqueue() {
+        return this.#list.pop() 
+    }
+
+    hasNext() {
+        return this.#list.length > 0;
+    }
+}
 
 module.exports = { Stack, Queue };
